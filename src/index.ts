67d97c0024f42e3e23e1a3de019cc9b2f09cf3cf
serafettin <mail@serafettin.net>
2023-03-26 19:43:19 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -71,6 +71,7 @@ export const litActionSignAndGetSignature = async (
     ...(context.litCode === undefined || ! context.litCode) && {ipfsId: context.ipfsId},
     authSig,
     jsParams,
+    targetNodeRange: 1
   }
 
   const res = await client.executeJs(executeOptions)
